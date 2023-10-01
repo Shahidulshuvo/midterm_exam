5ed0d332c508d2817e86f388b64856439a74c6a3
Shahidul Islam <shislam.student@wust.edu>
2023-10-01 14:52:59 -0400

    new

--- src/datastructure/UseQueue.java
+++ src/datastructure/UseQueue.java
@@ -1,14 +1,47 @@
 package datastructure;
-
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Iterator;
 public class UseQueue {
 
 	public static void main(String[] args) {
 		/*
 		 * Demonstrate how to use Queue that includes add,peek,remove,pool elements.
 		 * Use For Each loop and while loop with Iterator to retrieve data.
-		 * 
+		 *
 		 */
+		// Creating a Queue
+		Queue<String> javaQueue = new LinkedList<>();
 
+		// Add data to queue
+		javaQueue.add("Mercury");
+		javaQueue.add("Venus");
+		javaQueue.add("Earth");
+		javaQueue.add("Mars");
+		javaQueue.add("Jupiter");
+		javaQueue.add("Saturn");
+		javaQueue.add("Uranus");
+		javaQueue.add("Neptune");
+
+		// Demonstrate Queue operations
+		System.out.println("Queue of planets in the solar system: " + javaQueue);
+		System.out.println("Peek (get element at the front of the queue): " + javaQueue.peek());
+		System.out.println("Remove (remove and return the element at the front): " + javaQueue.remove());
+		System.out.println("Queue after removal: " + javaQueue);
+		System.out.println("Poll (remove and return the element at the front): " + javaQueue.poll());
+		System.out.println("Queue after poll: " + javaQueue);
+
+		// Use for-each loop to retrieve data
+		System.out.println("\nUsing for-each loop:");
+		for (String planet : javaQueue) {
+			System.out.println(planet);
+		}
+
+		// Use while loop with Iterator to retrieve data
+		System.out.println("\nUsing while loop with Iterator:");
+		Iterator<String> iterator = javaQueue.iterator();
+		while (iterator.hasNext()) {
+			System.out.println(iterator.next());
+		}
 	}
-
 }
--- src/math/problems/Factorial.java
+++ src/math/problems/Factorial.java
@@ -1,5 +1,5 @@
 package math.problems;
-
+import java.util.Scanner;
 public class Factorial {
 
     public static void main(String[] args) {
@@ -8,6 +8,67 @@ public class Factorial {
          * Write a java program to find Factorial of a given number using Recursion as well as Iteration.
          *
          */
+        //Utilize scanner to input value
+        Scanner scanner1 = new Scanner(System.in);
+        //Utilize repeat to repeat the code once an instance is finished
+        char repeat;
+        //Keep in do while loop for the code to repeat based on user input
+        do {
+            //Utilize long variable as answer will be larger than integer
+            long number;
+            //do while loop to repeat the user input
+            do {
+                System.out.print("Enter a positive integer to calculate its factorial: ");
+                while (!scanner1.hasNextLong()) {
+                    System.out.println("Invalid input. Please enter a positive integer.");
+                    scanner1.next(); // Consume the invalid input
+                }
+                number = scanner1.nextLong();
+                //Utilize if statement to keep user input as a positive value
+                if (number < 0) {
+                    System.out.println("Please enter a positive integer.");
+                }
+            } while (number < 0);
 
+            // factorial using recursion
+            long factorialRecursive = calculateFactorialRecursive(number);
+            System.out.println("Factorial using recursion: " + factorialRecursive);
+
+            // factorial using iteration
+            long factorialIterative = calculateFactorialIterative(number);
+            System.out.println("Factorial using iteration: " + factorialIterative);
+            //Utilize while statement to execute part of code while asking user if they would like to continue with another value
+            while (true) {
+                System.out.print("Do you want to enter another number? (y/n): ");
+                repeat = scanner1.next().charAt(0);
+                if (repeat == 'y' || repeat == 'n') {
+                    break;
+                } else {
+                    System.out.println("Invalid input. Please enter 'y' or 'n'.");
+                }
+            }
+
+        } while (repeat == 'y');
+
+        System.out.println("Goodbye!");
+        scanner1.close();
+    }
+
+    // Recursive method to calculate factorial
+    public static long calculateFactorialRecursive(long n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        } else {
+            return n * calculateFactorialRecursive(n - 1);
+        }
+    }
+
+    // Iterative method to calculate factorial
+    public static long calculateFactorialIterative(long n) {
+        long factorial = 1;
+        for (long i = 1; i <= n; i++) {
+            factorial *= i;
+        }
+        return factorial;
     }
 }
